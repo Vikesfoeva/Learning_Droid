124afa82920bba8aed2fbfc105e85c57e1f0423b
Brandon Lenz <lenzb@oregonstate.edu>
2020-10-11 22:28:21 -0400

    Started DNA Project

---
+++ DNA-String-Project-CodeAcademy-Project/DNA.java
@@ -0,0 +1,10 @@
+public class DNA {
+// Determines if a given sring of DNA represents a protein
+  String dna1 = "ATGCGATACGCTTGA";
+  String dna2 = "ATGCGATACGTGA";
+  String dna3 = "ATTAATATGTACTGA";
+
+  public static void main(String[] args) {
+
+  }
+}
