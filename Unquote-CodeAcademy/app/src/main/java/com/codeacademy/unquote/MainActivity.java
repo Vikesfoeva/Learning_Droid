f716695e9ed2d7cec7501b4d576a107bd80aebcf
Brandon Lenz <lenzb@oregonstate.edu>
2020-10-15 21:14:33 -0400

    Pushing to git for test

---
+++ Unquote-CodeAcademy/app/src/main/java/com/codeacademy/unquote/MainActivity.java
@@ -0,0 +1,15 @@
+package com.codeacademy.unquote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// started the project
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
